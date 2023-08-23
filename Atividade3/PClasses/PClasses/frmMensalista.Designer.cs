b75eef5375c091e172ce774798ef0eff894585bc
GGA <102376817+GGA-0@users.noreply.github.com>
2023-08-23 18:17:47 -0300

    Add files via upload

---
+++ Atividade3/PClasses/PClasses/frmMensalista.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace PClasses
+{
+    partial class frmMensalista
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMatricula = new System.Windows.Forms.TextBox();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbtnSim = new System.Windows.Forms.RadioButton();
+            this.rbtnNao = new System.Windows.Forms.RadioButton();
+            this.btnInstanciar1 = new System.Windows.Forms.Button();
+            this.btnInstanciar2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Matrícula";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nome";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(86, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Salário Mensal";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(86, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(217, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Data de Entrada na Empresa";
+            // 
+            // txtMatricula
+            // 
+            this.txtMatricula.Location = new System.Drawing.Point(333, 67);
+            this.txtMatricula.Name = "txtMatricula";
+            this.txtMatricula.Size = new System.Drawing.Size(100, 26);
+            this.txtMatricula.TabIndex = 6;
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(333, 224);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(148, 26);
+            this.txtData.TabIndex = 7;
+            this.txtData.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(333, 172);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(148, 26);
+            this.txtSalario.TabIndex = 8;
+            this.txtSalario.TextChanged += new System.EventHandler(this.TextBox3_TextChanged);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(333, 119);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(285, 26);
+            this.txtNome.TabIndex = 9;
+            this.txtNome.TextChanged += new System.EventHandler(this.TextBox4_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbtnNao);
+            this.groupBox1.Controls.Add(this.rbtnSim);
+            this.groupBox1.Location = new System.Drawing.Point(655, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(163, 174);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Trabalha em Home Office";
+            this.groupBox1.Enter += new System.EventHandler(this.GroupBox1_Enter);
+            // 
+            // rbtnSim
+            // 
+            this.rbtnSim.AutoSize = true;
+            this.rbtnSim.Location = new System.Drawing.Point(18, 73);
+            this.rbtnSim.Name = "rbtnSim";
+            this.rbtnSim.Size = new System.Drawing.Size(63, 24);
+            this.rbtnSim.TabIndex = 0;
+            this.rbtnSim.TabStop = true;
+            this.rbtnSim.Text = "SIM";
+            this.rbtnSim.UseVisualStyleBackColor = true;
+            this.rbtnSim.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
+            // 
+            // rbtnNao
+            // 
+            this.rbtnNao.AutoSize = true;
+            this.rbtnNao.Location = new System.Drawing.Point(18, 108);
+            this.rbtnNao.Name = "rbtnNao";
+            this.rbtnNao.Size = new System.Drawing.Size(68, 24);
+            this.rbtnNao.TabIndex = 1;
+            this.rbtnNao.TabStop = true;
+            this.rbtnNao.Text = "NÂO";
+            this.rbtnNao.UseVisualStyleBackColor = true;
+            this.rbtnNao.CheckedChanged += new System.EventHandler(this.RadioButton2_CheckedChanged);
+            // 
+            // btnInstanciar1
+            // 
+            this.btnInstanciar1.Location = new System.Drawing.Point(90, 342);
+            this.btnInstanciar1.Name = "btnInstanciar1";
+            this.btnInstanciar1.Size = new System.Drawing.Size(275, 146);
+            this.btnInstanciar1.TabIndex = 11;
+            this.btnInstanciar1.Text = "Instanciar Mensalista";
+            this.btnInstanciar1.UseVisualStyleBackColor = true;
+            this.btnInstanciar1.Click += new System.EventHandler(this.BtnInstanciar1_Click);
+            // 
+            // btnInstanciar2
+            // 
+            this.btnInstanciar2.Location = new System.Drawing.Point(543, 342);
+            this.btnInstanciar2.Name = "btnInstanciar2";
+            this.btnInstanciar2.Size = new System.Drawing.Size(275, 146);
+            this.btnInstanciar2.TabIndex = 12;
+            this.btnInstanciar2.Text = "Instanciar Mensalista passando parâmetros";
+            this.btnInstanciar2.UseVisualStyleBackColor = true;
+            this.btnInstanciar2.Click += new System.EventHandler(this.BtnInstanciar2_Click);
+            // 
+            // frmMensalista
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 549);
+            this.Controls.Add(this.btnInstanciar2);
+            this.Controls.Add(this.btnInstanciar1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.txtMatricula);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMensalista";
+            this.Text = "frmMensalista";
+            this.Load += new System.EventHandler(this.FrmMensalista_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMatricula;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbtnSim;
+        private System.Windows.Forms.RadioButton rbtnNao;
+        private System.Windows.Forms.Button btnInstanciar1;
+        private System.Windows.Forms.Button btnInstanciar2;
+    }
+}
---
+++ Atividade3/TAPWM/PClasses/PClasses/frmMensalista.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PClasses
+{
+    public partial class frmMensalista : Form
+    {
+        public frmMensalista()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmMensalista_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GroupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnInstanciar1_Click(object sender, EventArgs e)
+        {
+            Mensalista objMensalista = new Mensalista();
+            objMensalista.Matricula = Convert.ToInt32(txtMatricula.Text);
+            objMensalista.NomeEmpregado = txtNome.Text;
+            objMensalista.SalarioMensal = Convert.ToInt32(txtSalario.Text);
+            objMensalista.DataEntradaEmpresa = Convert.ToDateTime(txtData.Text);
+            if (rbtnSim.Checked)
+                objMensalista.HomeOffice = 'S';
+            else
+                objMensalista.HomeOffice = 'N';
+
+            MessageBox.Show("Matrícula: " + objMensalista.Matricula + "\n" +
+            "Nome: " + objMensalista.NomeEmpregado + "\n" +
+            "Data Entrada: " +
+            objMensalista.DataEntradaEmpresa.ToShortDateString() + 
+            "\n" + 
+            "Salário Bruto: " + objMensalista.SalarioBruto().ToString("N2")
+            + "\n" + 
+            "Tempo Empresa(dias): " + objMensalista.TempoTrabalho() +
+            '\n' + objMensalista.VerificaHome());
+        }
+
+        
+
+        private void BtnInstanciar2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
